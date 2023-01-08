1ee0c18905eabb5e9a8f55f682c03a2950465ac0
Chao Zhou <Czhou0k@gmail.com>
2023-01-08 09:27:32 -0500

    added edge cases

--- test_app.py
+++ test_app.py
@@ -5,8 +5,10 @@ import app
 class TestApp(unittest.TestCase):
 
   def test_add(self):
-    result = app.add(9,6)
-    self.assertEqual(result, 15)
+    self.assertEqual(app.add(9,6), 15)
+    self.assertEqual(app.add(8,5), 13)
+    self.assertEqual(app.add(-1,-1),-2)
+    self.assertEqual(app.add(100,5), 105)
 
 if __name__ == '__main__':
   unittest.main()
