c6424e655b06132cf0dffaeb837d675cb0c73528
Chao Zhou <Czhou0k@gmail.com>
2023-01-08 09:45:04 -0500

    added a type method

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ def add(x, y):
     return x + y
 
 
+def types(userid, time_stamp, latitude,longitude, speed):
+    return {
+        userid: '123',
+        time_stamp: 132343,
+        latitude: 342,
+        longitude: 167442,
+        speed: None
+    }
+
