cc75bbf0c9deac82741e1deae0fef248e190e2b4
Chao Zhou <Czhou0k@gmail.com>
2023-01-12 20:10:05 -0500

    added condition statements to boolean

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def types(userid, time_stamp, latitude,longitude, speed):
         speed: None
     }
 
-def boolean():
+def boolean(arg):
+    if arg == 1:
+        return "It is Number 1"
+    else:
+        return "It is not Number 1 "
 
 
